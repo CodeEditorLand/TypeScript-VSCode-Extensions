5d1a5474a541f213a4cf2dfa22be94df090ce40d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-27 17:01:31 +0300

    squash!

--- packages/typescript-playground/Source/web/sidebar/webviewProvider.ts
+++ packages/typescript-playground/Source/web/sidebar/webviewProvider.ts
@@ -1,75 +1,60 @@
-import * as vscode from "vscode";
-import type { Uri, Webview, WebviewViewProvider } from "vscode";
-import type { VFS } from "../../lib/VFS";
+import * as vscode from "vscode"
+import {
+  Uri,
+  Webview,
+  WebviewViewProvider,
+} from "vscode";
+import { VFS } from "../../lib/VFS";
 import { getUri } from "./getUri";
 
+
 export class Sidebar implements WebviewViewProvider {
-	public static readonly viewType =
-		"vscode-typescript-playground.sidebarView";
-	private _view?: vscode.WebviewView;
+  public static readonly viewType = 'vscode-typescript-playground.sidebarView';
+  private _view?: vscode.WebviewView;
 
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-		private readonly vfs: VFS,
-	) {}
+  constructor(private readonly _extensionUri: vscode.Uri, private readonly vfs: VFS) { }
 
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken,
+  ) {
 
-		webviewView.webview.options = {
-			enableScripts: true,
+    this._view = webviewView;
 
-			localResourceRoots: [this._extensionUri],
-		};
+    webviewView.webview.options = {
+      enableScripts: true,
 
-		webviewView.webview.html = this._getWebviewContent(
-			webviewView.webview,
-			this._extensionUri,
-		);
+      localResourceRoots: [
+        this._extensionUri
+      ]
+    };
 
-		webviewView.webview.onDidReceiveMessage((data) => {
-			console.log("Received", { data });
-			switch (data.type) {
-				case "ts-ready": {
-					if (!this._view) return;
-					this._view.webview.postMessage({
-						command: "updateTS",
-						ts: this.vfs.readFile(vscode.Uri.parse("/index.tsx")),
-					});
+    webviewView.webview.html = this._getWebviewContent(webviewView.webview, this._extensionUri);
 
-					break;
-				}
-				case "colorSelected": {
-					vscode.window.activeTextEditor?.insertSnippet(
-						new vscode.SnippetString(`#${data.value}`),
-					);
-					break;
-				}
-			}
-		});
-	}
+    webviewView.webview.onDidReceiveMessage(data => {
+      console.log("Received", { data })
+      switch (data.type) {
+        case "ts-ready": {
+          if (!this._view) return
+          this._view.webview.postMessage({ command: 'updateTS', ts: this.vfs.readFile(vscode.Uri.parse("/index.tsx")) });
 
-	private _getWebviewContent(webview: Webview, extensionUri: Uri) {
-		const mainUri = getUri(webview, extensionUri, [
-			"dist",
-			"web",
-			"webview.js",
-		]);
-		const stylesUri = getUri(webview, extensionUri, [
-			"src",
-			"webview",
-			"style.css",
-		]);
-		const version = "4.5.2";
-		const ts =
-			"https://typescript.azureedge.net/cdn/" +
-			version +
-			"/typescript/lib/typescript.js";
-		return `
+          break
+        }
+        case 'colorSelected': {
+          vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+          break;
+        }
+      }
+    });
+  }
+
+  private _getWebviewContent(webview: Webview, extensionUri: Uri) {
+    const mainUri = getUri(webview, extensionUri, ["dist", "web", "webview.js"]);
+    const stylesUri = getUri(webview, extensionUri, ["src", "webview", "style.css"]);
+    const version = "4.5.2"
+    const ts = "https://typescript.azureedge.net/cdn/" + version + "/typescript/lib/typescript.js"
+    return `
               <!DOCTYPE html>
               <html lang="en">
                   <head>
@@ -91,12 +76,12 @@ export class Sidebar implements WebviewViewProvider {
                   </body>
               </html>
           `;
-	}
+  }
 
-	updateTS(ts: string, diags: vscode.Diagnostic[]) {
-		if (this._view) {
-			this._view.show?.(true);
-			this._view.webview.postMessage({ command: "updateTS", ts, diags });
-		}
-	}
+  updateTS(ts: string, diags: vscode.Diagnostic[]) {
+    if (this._view) {
+      this._view.show?.(true);
+      this._view.webview.postMessage({ command: 'updateTS', ts, diags });
+    }
+  }
 }
